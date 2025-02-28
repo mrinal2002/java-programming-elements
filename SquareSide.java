2c38a456760dc4ea059b0bd101b255b5a0d048d4
Mrinal Patra <mrinal2002patra@gmail.com>
2025-02-28 22:10:52 +0530

    [Mrinal] feat: find square side from perimeter

---
+++ SquareSide.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter perimeter of the square: ");
+        double perimeter = sc.nextDouble();
+        double side = perimeter / 4;
+        System.out.printf("The length of the side is %.2f whose perimeter is %.2f\n", side, perimeter);
+    }
+}
