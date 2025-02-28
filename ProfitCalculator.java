09cea6e9368bd8d31fb2761e1a82b8a29e91f4ce
Mrinal Patra <mrinal2002patra@gmail.com>
2025-02-28 22:04:52 +0530

    [Mrinal] feat: calculate profit and profit percentage

---
+++ ProfitCalculator.java
@@ -0,0 +1,9 @@
+public class ProfitCalculator {
+    public static void main(String[] args) {
+        double costPrice = 129, sellingPrice = 191;
+        double profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / costPrice) * 100;
+        System.out.printf("The Cost Price is INR %.2f and Selling Price is INR %.2f\n", costPrice, sellingPrice);
+        System.out.printf("The Profit is INR %.2f and the Profit Percentage is %.2f%%\n", profit, profitPercentage);
+    }
+}
