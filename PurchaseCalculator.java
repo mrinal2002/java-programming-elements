773f44c55d778da7f494c3e23175d6540c56576e
Mrinal Patra <mrinal2002patra@gmail.com>
2025-02-28 22:12:18 +0530

    [Mrinal] feat: calculate total purchase price

---
+++ PurchaseCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PurchaseCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter unit price: ");
+        double unitPrice = sc.nextDouble();
+        System.out.print("Enter quantity: ");
+        int quantity = sc.nextInt();
+        double totalPrice = unitPrice * quantity;
+        System.out.printf("The total purchase price is INR %.2f if the quantity is %d and unit price is INR %.2f\n", totalPrice, quantity, unitPrice);
+    }
+}
